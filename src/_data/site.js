f7ba92d9e32ab91abc613e37266d2641339f0bfa
Jérôme Coupé <jercoupe@gmail.com>
2021-09-02 14:49:25 +0200

    CSS tweaks, RWD images and social media

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,7 @@
 module.exports = {
   title: "Equal Day 2021",
-  description: "Equal Day 2021 - Inventer le Futur",
+  description:
+    "Comment s’imaginer un avenir, lorsqu’il ne repose plus sur l’évidence du progrès? Comment tout simplement se figurer un futur, entre déni des enjeux et crainte de l’effondrement?",
   url: "https://www.equalday.eu",
   metaImage: "https://www.equalday.eu/assets/img/socialmedia.jpg",
   authorName: "Equal Partners",
